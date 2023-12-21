9ed8b504688503a86b6d47862c2e2de5ed761816
Prathamesh Narkhede <55591622+prathameshnarkhede@users.noreply.github.com>
2023-12-21 02:06:37 -0600

    Add project files.

---
+++ ArcGIS/ArcGIS/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI.Controls;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ArcGIS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "<API Key>";
+
+        }
+    }
+
+}
